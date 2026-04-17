50475c0e589f6a10f2d04012054464ff1d7d0fde
Andres Chavez <t4t4x777@gmail.com>
2026-04-16 21:05:37 -0400

    building the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <Card className="max-w-sm">
+      <CardHeader>
+        <CardTitle>Project Overview</CardTitle>
+        <CardDescription>
+          Track progress and recent activity for your Vite app.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        Your design system is ready. Start building your next component.
+      </CardContent>
+      <Button>hola</Button>
+    </Card>
+  )
+}
+
+export default App
