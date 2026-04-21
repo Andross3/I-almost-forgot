b52910ceb0f6a8ad33266c666ea69fa9ca5d4664
Andres Chavez <t4t4x777@gmail.com>
2026-04-21 19:43:31 -0400

    fix: module birthday person

--- backend/src/birthday_person/birthday_person.controller.ts
+++ backend/src/birthday_person/birthday_person.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { BirthdayPersonService } from './birthday_person.service';
+import { CreateBirthdayDto } from './dto/create-birthday.dto';
 
-@Controller('birthday-person')
+@Controller('/birthday-person')
 export class BirthdayPersonController {
   constructor(private birthdayPersonService: BirthdayPersonService){}
 
-  @Get('/sofia')
+  @Get('/birthdays')
   async findAll() {
     return this.birthdayPersonService.getAllBirthdayPeople();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.birthdayPersonService.getBirthdayPersonById(id);
+  }
+
+  @Post('/create-birthday')
+  async createBirthday(@Body() data: CreateBirthdayDto) {
+    return this.birthdayPersonService.createBirthdayPerson(data);
+  }
+
+  @Put(':id')
+  async updateBirthday(@Param('id') id: string, @Body() data:any) {
+    return this.birthdayPersonService.updateBirthdayPerson(id, data);
+  }
+
+  @Delete(':id')
+  async deleteBirthday(@Param('id') id: string) {
+    return this.birthdayPersonService.deleteBirthdayPerson(id);
   }
 }
--- backend/src/birthday_person/birthday_person.service.ts
+++ backend/src/birthday_person/birthday_person.service.ts
@@ -1,12 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBirthdayDto } from './dto/create-birthday.dto';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class BirthdayPersonService {
+  constructor(private prisma: PrismaService) {}
 
-  constructor(private prisma: PrismaService) { }
-
+  // obtener todos los cumpleaños
   async getAllBirthdayPeople() {
-    return this.prisma.birthday_person.findMany()
+    try {
+      const allBirthday = await this.prisma.birthday_person.findMany();
+      return {
+        message: 'Se obtuvieron todos los cumpleaños exitosamente',
+        data: allBirthday,
+      };
+    } catch (error) {
+      throw new BadRequestException('Error al obtener todos los cumpleaños');
+    }
+  }
+
+  // obtener un cumpleaños por id
+  async getBirthdayPersonById(id: string) {
+    return await this.findBirthdayOrFail(id);
+  }
+
+  // crear cumpleanos
+  async createBirthdayPerson(data: CreateBirthdayDto) {
+    try {
+      const birthday = await this.prisma.birthday_person.create({
+        data,
+      });
+      return {
+        message: 'Cumpleaños creado exitosamente',
+        data: birthday,
+      };
+    } catch (error) {
+      throw new BadRequestException('Error al crear cumpleaños');
+    }
+  }
+
+  // eliminar cumpleanos
+  async deleteBirthdayPerson(id: string) {
+
+    await this.findBirthdayOrFail(id)
+
+    try {
+      const birthday = await this.prisma.birthday_person.delete({
+        where: { id },
+      });
+      return {
+        message: 'Cumpleaños eliminado exitosamente',
+        data: birthday,
+      };
+    } catch (error) {
+      throw new BadRequestException('Error al eliminar cumpleaños');
+    }
+  }
+
+  // editar cumpleanos
+  async updateBirthdayPerson(id: string, data: any) {
+    // buscar cumpleaños por id
+    await this.findBirthdayOrFail(id)
+
+    try {
+      const birthday = await this.prisma.birthday_person.update({
+        where: { id },
+        data,
+      });
+
+      return {
+        message: 'Cumpleaños actualizado exitosamente',
+        data: birthday,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException('Error al actualizar cumpleaños')
+    }
+  }
+
+  // buscar cumpleaños por id
+  private async findBirthdayOrFail(id: string) {
+    const birthday = await this.prisma.birthday_person.findUnique({
+      where: { id },
+    });
+
+    if (!birthday) {
+      throw new NotFoundException('Cumpleaños no encontrado');
+    }
+
+    return birthday;
   }
 }
