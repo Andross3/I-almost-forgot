71700a79123731357cb85ab0eb2d3e3795506de6
Andres Chavez <t4t4x777@gmail.com>
2026-04-20 21:25:23 -0400

    feat: add dependencies and configure the database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { BirthdayPersonModule } from './birthday_person/birthday_person.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), BirthdayPersonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
