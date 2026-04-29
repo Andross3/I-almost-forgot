ca4e52c0b46d91d0fd421e50df1eba9d0dfdffb5
Andres Chavez <t4t4x777@gmail.com>
2026-04-29 19:30:49 -0400

    pointing to the dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,8 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
+import Dashboard from "@/pages/dashboard";
 
 function App() {
   return (
-    <Card className="max-w-sm">
-      <CardHeader>
-        <CardTitle>Project Overview</CardTitle>
-        <CardDescription>
-          Track progress and recent activity for your Vite app.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        Your design system is ready. Start building your next component.
-      </CardContent>
-      <Button>hola</Button>
-    </Card>
+    <Dashboard />
   )
 }
 
